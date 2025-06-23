aa639b827637ab6548c3d1543cf81dcf70985146
Rodwan Bagdadi <rodwanbagdadi@gmail.com>
2025-06-23 23:04:56 +0300

    feat: add vite-plugin-svgr for SVG handling
    - Updated package.json to include vite-plugin-svgr.
    - Modified vite-env.d.ts to declare SVG imports as React components.
    - Updated vite.config.ts to include the SVGR plugin in the Vite configuration.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
 
 // Allow SVG imports
 declare module "*.svg" {
   import React = require("react");
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  import { SVGProps } from "react";
+  const ReactComponent: React.FC<SVGProps<SVGSVGElement>>;
   const src: string;
   export default src;
   export { ReactComponent };
